bb1c5cdd6c0b68f934ca2252169bd884183e617e
IcaroRaphael <icaro2551@gmail.com>
2023-09-07 21:36:33 -0300

    feat: Abstração

---
+++ Pilares-da-Programacao-Orientada-a-Objetos-em-Java/src/abstracao/ComputadorPedrinho.java
@@ -0,0 +1,22 @@
+package abstracao;
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+
+        System.out.println("MSN MESSENGER:");
+        MsnMessenger msn = new MsnMessenger();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+
+        System.out.println("FACEBOOK MESSENGER:");
+        FacebookMessenger fbm = new FacebookMessenger();
+        fbm.enviarMensagem();
+        fbm.receberMensagem();
+
+        System.out.println("TELEGRAM:");
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
+
+    }
+}

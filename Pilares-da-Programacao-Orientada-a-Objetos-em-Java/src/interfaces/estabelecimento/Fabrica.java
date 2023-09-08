4e8bf1503253e9c3d6c24913a4a85c9d25f6bc6b
IcaroRaphael <icaro2551@gmail.com>
2023-09-07 22:54:50 -0300

    feat: Interface

---
+++ Pilares-da-Programacao-Orientada-a-Objetos-em-Java/src/interfaces/estabelecimento/Fabrica.java
@@ -0,0 +1,34 @@
+package interfaces.estabelecimento;
+
+import interfaces.equipamentos.copiadora.Copiadora;
+import interfaces.equipamentos.copiadora.Xerox;
+import interfaces.equipamentos.digitalizadora.Digitalizadora;
+import interfaces.equipamentos.digitalizadora.Scanner;
+import interfaces.equipamentos.impressora.Deskjet;
+import interfaces.equipamentos.impressora.Impressora;
+import interfaces.equipamentos.impressora.Laserjet;
+import interfaces.equipamentos.multifuncional.EquipamentoMultifuncional;
+
+public class Fabrica {
+    public static void main(String[] args) {
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+
+        System.out.println("-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-");
+
+        Deskjet deskjet = new Deskjet();
+        Scanner scanner = new Scanner();
+        Xerox xerox = new Xerox();
+        impressora = deskjet;
+        digitalizadora = scanner;
+        copiadora = xerox;
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+    }
+}
